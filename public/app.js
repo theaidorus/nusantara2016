8411ad94532dbb406c5267485cc1b821f57eb430
Calvin Handoko <chandoko@berkeley.edu>
2016-01-30 17:12:15 -0800

    Page 1 Done

--- public/app.js
+++ public/app.js
@@ -1,3 +1,8 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+<script src="js/skrollr.js"></script>
+<script src="js/_main.js"></script>
+
 			$('#satu').click(function() {
 				$(this).addClass("fadeIn");
 			});
